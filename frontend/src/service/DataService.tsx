c2a30490a69160fdedfa25e3c294dcd54b52f241
MurakamiHonami <mnv2525@gmail.com>
2026-02-15 14:53:02 +0900

    add config.tsx

--- frontend/src/service/DataService.tsx
+++ frontend/src/service/DataService.tsx
@@ -1,7 +1,8 @@
 import { Data } from "../types/Data";
+import { API_URL } from "../components/config";
 
 export const fetchData = async (): Promise<Data[]> => {
-    const response = await fetch("http://localhost:5000/api/data");
+    const response = await fetch(`${API_URL}/api/data`);
     if (!response.ok) {
         throw new Error("データの取得に失敗しました");
     }
